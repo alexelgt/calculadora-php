3d65861b55ee746a2b7ec0385f6e1d28c7ffb583
alexelgt <alexelgt@gmail.com>
2021-12-26 17:26:25 +0100

    Añadido fichero Calculadora.php

---
+++ Calculadora.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Clase Calculadora
+ * 
+ * Esta clase realiza las operaciones de aritmética básicas de suma, resta, multiplicación y división.
+ * 
+ * @author Alexelgt
+ * @version 0.1
+ */
+class Calculadora {
+    
+}
+
