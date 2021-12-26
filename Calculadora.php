15484c5eea8cae077c3725b57d0d6c4e1bec391f
alexelgt <alexelgt@gmail.com>
2021-12-26 17:52:08 +0100

    Añadidos métodos multiplicacion y division

--- Calculadora.php
+++ Calculadora.php
@@ -6,7 +6,7 @@
  * Esta clase realiza las operaciones de aritmética básicas de suma, resta, multiplicación y división.
  * 
  * @author Alexelgt
- * @version 0.2
+ * @version 0.3
  */
 class Calculadora {
     /**
@@ -29,6 +29,28 @@ class Calculadora {
     */
    public function resta($a, $b) {
        return $a - $b;
+   }
+   
+   /**
+    * Realiza la multiplicación de 2 números
+    * 
+    * @param int $a Valor que representa el primer operando
+    * @param int $b Valor que representa el segundo operando
+    * @return int Resultado de la operación
+    */
+   public function multiplicacion($a, $b) {
+       return $a * $b;
+   }
+   
+   /**
+    * Realiza la división de 2 números
+    * 
+    * @param int $a Valor que representa el primer operando
+    * @param int $b Valor que representa el segundo operando
+    * @return int Resultado de la operación
+    */
+   public function division($a, $b) {
+       return $a / $b;
    }
 }
 
