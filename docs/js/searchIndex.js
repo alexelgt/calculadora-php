1274850dfa36be5cff09f87151efb207f6982d7f
alexelgt <alexelgt@gmail.com>
2021-12-26 17:55:30 +0100

    Añadida documentación

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asuma\u0028\u0029",
+            "name": "suma",
+            "summary": "Realiza\u0020la\u0020suma\u0020de\u00202\u0020n\u00FAmeros",
+            "url": "classes/Calculadora.html#method_suma"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Aresta\u0028\u0029",
+            "name": "resta",
+            "summary": "Realiza\u0020la\u0020resta\u0020de\u00202\u0020n\u00FAmeros",
+            "url": "classes/Calculadora.html#method_resta"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Amultiplicacion\u0028\u0029",
+            "name": "multiplicacion",
+            "summary": "Realiza\u0020la\u0020multiplicaci\u00F3n\u0020de\u00202\u0020n\u00FAmeros",
+            "url": "classes/Calculadora.html#method_multiplicacion"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Adivision\u0028\u0029",
+            "name": "division",
+            "summary": "Realiza\u0020la\u0020divisi\u00F3n\u0020de\u00202\u0020n\u00FAmeros",
+            "url": "classes/Calculadora.html#method_division"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
