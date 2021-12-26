ed37d29d3595a47cc8f74f4ce2029a7558763000
alexelgt <alexelgt@gmail.com>
2021-12-26 17:47:34 +0100

    Añadidos métodos suma y resta

--- Calculadora.php
+++ Calculadora.php
@@ -6,9 +6,29 @@
  * Esta clase realiza las operaciones de aritmética básicas de suma, resta, multiplicación y división.
  * 
  * @author Alexelgt
- * @version 0.1
+ * @version 0.2
  */
 class Calculadora {
-    
+    /**
+    * Realiza la suma de 2 números
+    * 
+    * @param int $a Valor que representa el primer operando
+    * @param int $b Valor que representa el segundo operando
+    * @return int Resultado de la operación
+    */
+   public function suma($a, $b) {
+       return $a + $b;
+   }
+   
+   /**
+    * Realiza la resta de 2 números
+    * 
+    * @param int $a Valor que representa el primer operando
+    * @param int $b Valor que representa el segundo operando
+    * @return int Resultado de la operación
+    */
+   public function resta($a, $b) {
+       return $a - $b;
+   }
 }
 
